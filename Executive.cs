b59506a394e0b116a70b2acf86a23f510491df3d
JennifayDean <37194709+JennifayDean@users.noreply.github.com>
2018-05-28 11:29:05 -0700

    Jathen'sAssignment8Changes

---
+++ Executive.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Employees
+{
+    // Executives have titles
+    public enum ExecTitle { CEO, CTO, CFO, VP }
+
+    [Serializable]
+    public class Executive : Manager
+    {
+		#region constructors 
+		// Executives start with Gold benefits and 10,000 stock options
+		public Executive() : base()
+        {
+            empBenefits = new GoldBenefitPackage();
+            StockOptions = 10000;
+        }
+
+		public Executive(string firstname, string lastname, DateTime DOB, float currPay, string ssn, int numbOfOpts = 10000, ExecTitle title = ExecTitle.VP)
+          : base(firstname, lastname, DOB, currPay, ssn, numbOfOpts)
+        {
+			// Title defined by the Executive class.
+			Title = title;
+            empBenefits = new GoldBenefitPackage();
+		}
+        #endregion
+
+        public ExecTitle Title { get; set; } = ExecTitle.VP;
+
+		public override void DisplayStats()
+		{
+			base.DisplayStats();
+			Console.WriteLine("Executive Title: {0}", Title);
+		}
+
+        // Executives get an extra 1000 bonus and 10,000 stock options on promotion
+        // Move GivePromotion
+        public override void GivePromotion()
+        {
+            base.GivePromotion();
+            GiveBonus(1000);
+            StockOptions += 10000;
+        }
+
+		// Methods for adding reports
+		public override void AddReport(Employee newReport)
+        {
+            // Check for proper report to Executive
+            if (newReport is Manager || newReport is SalesPerson)
+            {
+                base.AddReport(newReport);
+            }
+            else
+            {
+                // Raise exception for report that is not a Manager or Salesperson
+                Exception ex = new AddReportException("Executive report not a Manager or Salesperson");
+
+                // Add Manager custom data dictionary
+                ex.Data.Add("Manager", this.Name);
+
+                // Add report that failed to be added, and throw exception
+                ex.Data.Add("New Report", newReport.Name);
+                throw ex;
+            }            
+        }
+	}
+}
---
+++ SalesPerson.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+
+namespace Employees
+{
+    // Salespeople need to know their number of sales.
+    [Serializable]
+    class SalesPerson : Employee
+    {
+        #region constructors 
+        public SalesPerson() { }
+
+        // As a general rule, all subclasses should explicitly call an appropriate
+        // base class constructor.
+        public SalesPerson(string firstname, string lastname, DateTime DOB, float currPay, string ssn, int numbOfSales)
+          : base(firstname, lastname, DOB, currPay, ssn)
+        {
+            // This belongs with us!
+            SalesNumber = numbOfSales;
+        }
+        #endregion
+
+        public int SalesNumber { get; set; }
+
+        // A salesperson's bonus is influenced by the number of sales.
+        public override sealed void GiveBonus(float amount)
+        {
+            int salesBonus = 0;
+            if (SalesNumber >= 0 && SalesNumber <= 100)
+                salesBonus = 10;
+            else
+            {
+                if (SalesNumber >= 101 && SalesNumber <= 200)
+                    salesBonus = 15;
+                else
+                    salesBonus = 20;
+            }
+            base.GiveBonus(amount * salesBonus);
+        }
+
+        // A SalesPerson gets an extra 300 on promotion
+        public override sealed void GivePromotion()
+        {
+            base.GivePromotion();
+            GiveBonus(300);
+        }
+
+        public override void DisplayStats()
+        {
+            base.DisplayStats();
+            Console.WriteLine("Number of sales: {0:N0}", SalesNumber);
+        }
+    }
+}
